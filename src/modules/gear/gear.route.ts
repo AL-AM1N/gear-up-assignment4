23fbea67718f492fd2472eca785e20dda5e157e1
MD. AL-AMIN <amin57.cse@gmail.com>
2026-07-11 07:53:11 +0600

    add get all gear api with filter option

---
+++ src/modules/gear/gear.route.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { gearController } from "./gear.controller";
+
+const router = Router();
+
+router.get("/", gearController.getAllGear);
+
+
+export const gearRoutes = router;
